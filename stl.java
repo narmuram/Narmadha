6176656327cc2950def3ebd6905da32eec70cf55
narmuram <41137457+narmuram@users.noreply.github.com>
2018-07-22 14:22:43 +0530

    Create stl.java

---
+++ stl.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Positive {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String s1=s.next();
+		StringBuffer sb=new StringBuffer(s1);
+		sb.append(".");
+		System.out.println(sb);
+	}
+	}
+		
